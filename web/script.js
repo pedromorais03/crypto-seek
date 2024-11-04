0d44e1fec5ebb15eb0bd0fca76526d7683dfb06e
phmpereira <pedro.pereira@sptech.school>
2024-11-04 13:35:45 -0300

    changed js function

--- web/script.js
+++ web/script.js
@@ -2,12 +2,9 @@ let mobileMenu = document.querySelector('.mobile-menu')
 let navList = document.querySelector('.nav-list')
 let navLinks = document.querySelectorAll('.nav-link')
 
-
-function toggleNav(){
+mobileMenu.addEventListener('click', () => {
    navList.classList.toggle("active")
    navLinks.forEach((links) => {
       links.classList.toggle("active")
    })
-}
-
-mobileMenu.addEventListener('click', toggleNav)
+})
